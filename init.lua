ff5d8110d3c2accbdf14ff004e5b15e60abec1de
Jason <ikatsaros@clickup.com>
2026-05-24 12:54:15 -0400

    feat(keymaps): send d/D deletes to black-hole register

--- init.lua
+++ init.lua
@@ -184,6 +184,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Delete without yanking (send to black-hole register)
+vim.keymap.set({ 'n', 'v' }, 'd', '"_d', { desc = 'Delete without yanking' })
+vim.keymap.set({ 'n', 'v' }, 'D', '"_D', { desc = 'Delete to end of line without yanking' })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
