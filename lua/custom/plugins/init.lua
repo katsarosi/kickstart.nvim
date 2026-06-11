331ee7966eae4893a1ee73695b2506207922af41
Gandalf <jason.katsaros@fridayharbor.ai>
2026-06-11 09:47:25 -0400

    jpp_search points to repo, not local

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,31 +4,32 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    "stevearc/conform.nvim",
+    'stevearc/conform.nvim',
     -- load early so you can format on save right away
-    event = { "BufReadPre", "BufNewFile", "BufWritePre" },
+    event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
 
     -- Put the JS/TS indent override here so it always applies
     init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { 
-          "javascript",
-          "javascriptreact",
-          "json",
-          "typescript",
-          "typescriptreact",
-          "ruby",
-          "java",
-          "python",
-          "sh",
-          "sql",
-          "lua"},
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'javascript',
+          'javascriptreact',
+          'json',
+          'typescript',
+          'typescriptreact',
+          'ruby',
+          'java',
+          'python',
+          'sh',
+          'sql',
+          'lua',
+        },
         callback = function(args)
           local bo = vim.bo[args.buf]
-          bo.shiftwidth  = 2
-          bo.tabstop     = 2
+          bo.shiftwidth = 2
+          bo.tabstop = 2
           bo.softtabstop = 2
-          bo.expandtab   = true
+          bo.expandtab = true
         end,
       })
     end,
@@ -37,46 +38,43 @@ return {
     opts = {
       -- map filetypes to formatter chains
       formatters_by_ft = {
-        lua              = { "stylua" },
-        javascript       = { "prettierd", "prettier", "js_beautify", stop_after_first = true },
-        avascriptreact  = { "prettierd", "prettier", "js_beautify", stop_after_first = true },
-        json             = { "prettierd", "prettier",                stop_after_first = true },
-        typescript       = { "prettierd", "prettier", "js_beautify", stop_after_first = true },
-        ruby             = { "rubocop"},
-        python           = { "black"},
-        java             = { "google-java-format"},
-        sh               = { "shfmt"},
-        sql              = { "sql-formatter", "pg_format", stop_after_first = true },
-        csv              = { "prettier"}, 
+        lua = { 'stylua' },
+        javascript = { 'prettierd', 'prettier', 'js_beautify', stop_after_first = true },
+        avascriptreact = { 'prettierd', 'prettier', 'js_beautify', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', 'js_beautify', stop_after_first = true },
+        ruby = { 'rubocop' },
+        python = { 'black' },
+        java = { 'google-java-format' },
+        sh = { 'shfmt' },
+        sql = { 'sql-formatter', 'pg_format', stop_after_first = true },
+        csv = { 'prettier' },
         -- add more as needed...
       },
       -- automatically format on save (with a 500 ms timeout)
       format_on_save = {
         timeout_ms = 50000,
-        lsp_format  = "fallback",
+        lsp_format = 'fallback',
       },
 
       formatters = {
-        ["sql-formatter"] = {
-          command = "sql-formatter",
-          args = { "-l", "postgresql"},
+        ['sql-formatter'] = {
+          command = 'sql-formatter',
+          args = { '-l', 'postgresql' },
           stdin = true,
         },
-        shfmt = { prepend_args = { "-i", "2"}},
-      }
+        shfmt = { prepend_args = { '-i', '2' } },
+      },
     },
   },
   {
-     -- Use your actual path to the repo clone
-    dir = "~/Projects/NeoVimPlugins/jpp_search.nvim/",
-    --"https://github.com/katsarosi/jpp_search.nvim.git",
-    name = "jpp-search.nvim",
-    ft = { "json", "jsonc" },
-    version = "*",
+    'katsarosi/jpp_search.nvim',
+    name = 'jpp-search.nvim',
+    ft = { 'json', 'jsonc' },
+    version = '*',
     opts = {
-      -- bin = "/usr/local/bin/jpp", -- if jpp isn't on PATH
-      target = "float",          -- "quickfix" | "float" | "telescope"
-      flags = { "-p"},
+      target = 'float', -- "quickfix" | "float" | "telescope"
+      flags = { '-p' },
     },
   },
   --{
