3db3d96702ce9dfeb7f4dcd89f46f78ac6d59d5d
Iasonas (Jason) Katsaros <jason@fridayharbor.ai>
2026-09-24 10:24:20 -0400

    Add Mermaid support

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -90,4 +90,14 @@ return {
       },
     },
   },
+  {
+    'kevalin/mermaid.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('mermaid').setup()
+
+      -- Install the Tree-sitter parser:
+      -- :TSInstall mermaid
+    end,
+  },
 }
