2765b197fd5c91910ffb58ebab5db20d5dd33682
Iasonas (Jason) Katsaros <jason@fridayharbor.ai>
2026-09-24 10:23:58 -0400

    Configure Bash language server and formatter

--- init.lua
+++ init.lua
@@ -675,6 +675,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        bashls = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -719,6 +720,7 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'shfmt', -- Used to format shell scripts
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
